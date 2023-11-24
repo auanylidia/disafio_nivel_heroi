acff93d10942ebc049bed230b81c38e9a701a1f1
auany <auany.lidia@gmail.com>
2023-11-24 19:12:00 -0300

    meu primeiro commit

---
+++ projeto.js
@@ -0,0 +1,20 @@
+let NomeHeroi= "Aya";
+let NivelXp= 100;
+
+if (NivelXp<=1000){
+    console.log("O Herói de nome " + NomeHeroi + " esta no nível de Ferro");
+} else if ( NivelXp>= 1001 && NivelXp<=2000){
+    console.log("O Herói de nome " + NomeHeroi + " esta no nível de Bronze");
+} else if ( NivelXp>= 2001 && NivelXp<=5000){
+    console.log("O Herói de nome " + NomeHeroi + " esta no nível de Prata");
+} else if ( NivelXp>= 6001 && NivelXp<=7000){
+    console.log("O Herói de nome " + NomeHeroi + " esta no nível de Ouro");
+} else if ( NivelXp>= 7001 && NivelXp<=8000){
+    console.log("O Herói de nome " + NomeHeroi + " esta no nível de Platina");
+} else if ( NivelXp>= 8001 && NivelXp<=9000){
+    console.log("O Herói de nome " + NomeHeroi + " esta no nível de Ascendente");
+} else if ( NivelXp>= 9001 && NivelXp<=10000){
+    console.log("O Herói de nome " + NomeHeroi + " esta no nível de Imortal");
+} else if ( NivelXp>= 10001 ){
+    console.log("O Herói de nome " + NomeHeroi + " esta no nível de Radiante");
+}
